657e0e74649e0fbe7ebb9044bff3bc8fc49df73f
Áron Tompai <aron.tompai@stud.uni-corvinus.hu>
2020-12-03 22:09:11 +0100

    Web Services tesztelés a projekthez

--- IRF_Project_GQOTXA/IRF_Project_GQOTXA/Form1.Designer.cs
+++ IRF_Project_GQOTXA/IRF_Project_GQOTXA/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 356);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(750, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1031, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- IRF_Project_GQOTXA/IRF_Project_GQOTXA/Form1.cs
+++ IRF_Project_GQOTXA/IRF_Project_GQOTXA/Form1.cs
@@ -1,4 +1,5 @@
-﻿using IRF_Project_GQOTXA.MnbServiceReference;
+﻿using IRF_Project_GQOTXA.Entities;
+using IRF_Project_GQOTXA.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,31 +9,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_Project_GQOTXA
 {
     public partial class Form1 : Form
     {
+        BindingList<Adatok> Rates = new BindingList<Adatok>();
+
         public Form1()
         {
             InitializeComponent();
-            WebszolgaltatashivasaTest();
+            currenciesTest(); 
         }
 
-        public void WebszolgaltatashivasaTest()
+        public void currenciesTest()
+        {
+            comboBox1.Items.Clear();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetCurrenciesRequestBody();
+
+            var response = mnbService.GetCurrencies(request);
+
+            string result = response.GetCurrenciesResult;
+
+            int currCounter = 1;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            XmlNodeList nodes = xml.SelectNodes("/MNBCurrencies/Currencies/Curr");
+
+            foreach (XmlNode xn in nodes)
+            {
+                string Valuta = xn.InnerText;
+                comboBox1.Items.Add(Valuta);
+                currCounter++;
+            }
+
+            WebszolgaltatashivasaTest(currCounter);
+
+        }
+
+        public void WebszolgaltatashivasaTest(int currCnt)
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetCurrentExchangeRatesRequestBody()
-            {
-                currencyNames = "GBP",
-                startDate = "2020-10-01",
-                endDate = "2020-11-30"
-            };
+            var request = new GetCurrentExchangeRatesRequestBody();
 
-            var response = mnbService.GetExchangeRates(request);
+            var response = mnbService.GetCurrentExchangeRates(request);
 
-            var result = response.GetExchangeRatesResult;
+            var result = response.GetCurrentExchangeRatesResult;
+
+            dataGridView1.DataSource = Rates;
+
+            xmltest(result, currCnt);
         }
+
+        public void xmltest(string result, int currCnt)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                for (int child = 0; child <= currCnt; child++)
+                {
+                    var rate = new Adatok();
+
+                    rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+            
+                    var childElement = (XmlElement)element.ChildNodes[child];
+                    if (childElement == null)
+                    {
+                        continue;
+                    }
+                    rate.Currency = childElement.GetAttribute("curr");
+                    
+
+                    var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                    var value = decimal.Parse(childElement.InnerText);
+                    if (unit != 0)
+                        rate.Value = value / unit;
+
+                    Rates.Add(rate);
+                }
+
+            }
+        }
+
     }
 }
